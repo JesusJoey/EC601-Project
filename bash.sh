805e119446079ebf02eb7a6734ec81bcf7337798
JesusJoey <43118582+JesusJoey@users.noreply.github.com>
2018-09-19 16:52:29 -0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Copyright:Yang Qiao
 #Step1:donwload images from a twitter acount to your directory, you can choose the user as well as the number of pictures
-python tweepy_hw1.py --username kobebryant --num 100 
+python tweepy_hw1.py --username kobebryant --num 100 --folder images
 
 #Step2:convert images into video; setup some parameters
 ffmpeg -f image2 -r 1/5 -i /Users/joe/Desktop/BU_2018_fall/EC601/images/%02d.jpg -vcodec libx264 -pix_fmt yuv420p out.mp4
